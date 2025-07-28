b34b5f33bc1a41fb782d1c8110d76b5dfa082e65
ShaanSworanandaBaidya <shaan.mandalainfosys@gmail.com>
2025-07-28 11:43:19 +0545

    Warning Fixedd

--- juillet/app/(cartbtn)/products/[id]/page.tsx
+++ juillet/app/(cartbtn)/products/[id]/page.tsx
@@ -20,7 +20,7 @@ export async function generateStaticParams() {
 }
 const API_URL =
   process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
-const ProductPage = async ({ params }: Props) => {
+const ProductPage = async ({ params }) => {
   const productId = parseInt(params.id);
   const product = await fetchProductById(productId);
 
